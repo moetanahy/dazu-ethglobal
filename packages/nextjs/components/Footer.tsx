50daa1bff34fc09d780a210ece9f99b26214fc6d
Mahamad El Tanahy <moe@brightcreations.com>
2024-10-19 21:37:11 -0700

    cleaned up the ui a bit

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -46,12 +46,12 @@ export const Footer = () => {
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
-            <div className="text-center">
+            {/* <div className="text-center">
               <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
                 Fork me
               </a>
-            </div>
-            <span>·</span>
+            </div> */}
+            {/* <span>·</span>
             <div className="flex justify-center items-center gap-2">
               <p className="m-0 text-center">
                 Built with <HeartIcon className="inline-block h-4 w-4" /> at
@@ -71,7 +71,7 @@ export const Footer = () => {
               <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer" className="link">
                 Support
               </a>
-            </div>
+            </div> */}
           </div>
         </ul>
       </div>
